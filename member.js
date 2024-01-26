d990714715f8cad96071dc0f1411974c6d03ba08
rafareino <rafareino@gmail.com>
2024-01-26 18:25:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMembrer() {
+    var skills = ["HTML", "CSS", "Javascript", "PHP", "MySQL"];
+    var skillsMembrer = document.getElementById('skillsMembrer');
+    skillsMembrer.innerHTML = "";
+    for (var i = 0; i < skills.length; i++) {
+        skillsMembrer.innerHTML += "<li>" + skills[i] + "</li>";
+    }
+}
